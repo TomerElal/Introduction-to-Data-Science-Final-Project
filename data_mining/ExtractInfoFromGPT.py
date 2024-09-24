7245731a3760125bac9e1e4411b640de226f1f53
t-tomerelal <t-tomerelal@microsoft.com>
2024-09-24 11:24:27 +0300

    Cosmetic changes and final version.

--- data_mining/ExtractInfoFromGPT.py
+++ data_mining/ExtractInfoFromGPT.py
@@ -2,20 +2,24 @@ import openai
 from openai import OpenAI
 import os
 
-API_KEY = 'apikey'
+API_KEY = 'API_KEY'
 # Ensure the OPENAI_API_KEY environment variable is set
 os.environ['OPENAI_API_KEY'] = API_KEY
 client = OpenAI()
 
 
 def extract_info_from_gpt(post_content, prefix_prompt):
-    completion = openai.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {
-                "role": "user",
-                "content": prefix_prompt + post_content,
-            },
-        ],
-    )
-    return completion.choices[0].message.content
+    try:
+        completion = openai.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prefix_prompt + post_content,
+                },
+            ],
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        print(e)
+        return "Gpt Failed\nGpt Failed"
